d7d2053d9c8e6e123b47cbd4120ad942a61f85f5
Inaaaam <69984637+inaam10-tech@users.noreply.github.com>
2022-07-15 00:04:21 -0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-7xl font-bold inline border-b-4 border-pink-600'>
+            <p className='text-7xl font-bold inline border-b-4 border-pink-600 hover:text-9xl duration-300'>
               About
             </p>
           </div>
